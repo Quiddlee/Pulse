80200c3af6a54ac6b25ea869d405dcfa57a925ad
Bogdan <thegarrybit@gmail.com>
2022-11-06 00:18:16 +0200

    carousel section start

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.svg"></button>',
+        Responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+        ]
+      });
+  });
