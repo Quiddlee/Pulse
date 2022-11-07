79cb1e6598d8c08c5bfde821e2820b1ade4077a0
Bogdan <thegarrybit@gmail.com>
2022-11-07 19:29:24 +0200

    hovers added

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        speed: 200,
+        speed: 600,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
         responsive: [
