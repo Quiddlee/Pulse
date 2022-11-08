73b43d156e0c82f260424b03d7a41f020802c4f7
Bogdan <thegarrybit@gmail.com>
2022-11-08 19:23:46 +0200

    smooth page up & fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 600,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    dots: true,
-                    arrows: false,
-                }
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                dots: true,
+                arrows: false,
             }
-        ]
+        }]
     });
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass
-          ('catalog__content_active').eq($(this).index()).addClass
-          ('catalog__content_active');
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -37,14 +33,14 @@ $(document).ready(function(){
 
     //Modals
 
-    $('[data-modal=consultation]').on('click', function() {
+    $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('slow');
-    }); 
-    $('.modal__close').on('click', function() {
+    });
+    $('.modal__close').on('click', function () {
         $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         });
@@ -56,7 +52,7 @@ $(document).ready(function(){
                 name: {
                     required: true,
                     minlength: 2
-                  },
+                },
                 phone: 'required',
                 email: {
                     required: true,
@@ -67,11 +63,11 @@ $(document).ready(function(){
                 name: {
                     required: 'Пожаллуйста, введите своё имя',
                     minlength: jQuery.validator.format("Введите как минимум {0} символа!")
-                  },
+                },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
-                  required: "Пожалуйста, введите свою почту",
-                  email: "Введён неверный адрес почты"
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Введён неверный адрес почты"
                 }
             }
         });
@@ -82,4 +78,26 @@ $(document).ready(function(){
     valideForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    //Page up & smooth scroll
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 300, function(){
+            window.location.hash = hash;
+        });
+        }
+    });
 });
